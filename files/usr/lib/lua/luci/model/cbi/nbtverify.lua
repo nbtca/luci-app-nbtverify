e11eb40cbd7de8a9ba3c4185f050daabd1e95409
LazuliKao <LazuliKao233@outlook.com>
2024-09-15 23:25:53 +0800

    fix

--- files/usr/lib/lua/luci/model/cbi/nbtverify.lua
+++ files/usr/lib/lua/luci/model/cbi/nbtverify.lua
@@ -6,14 +6,10 @@ s = m:section(TypedSection, "server", "")
 s.addremove = false
 s.anonymous = true
 
-enable = s:option(Flag, "enable", translate("Enable"))
+enabled = s:option(Flag, "enabled", translate("Enable"))
 username = s:option(Value, "username", translate("Username"))
 pass = s:option(Value, "password", translate("Password"))
-is_mobile = s:option(Flag, "is_mobile", translate("Mobile"))
--- o = s:option(Value, "delay", translate("Delayed Start (seconds)"))
--- o.datatype = "and(uinteger,min(0))"
--- o.default = "60"
-
+mobile = s:option(Flag, "mobile", translate("Mobile"))
 pass.password = true
 m.apply_on_parse = true
 m.on_after_apply = function(self, map)
